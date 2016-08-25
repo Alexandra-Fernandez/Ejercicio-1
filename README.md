cd31618fc50fd4195f991f17796aa797c3210f93
Alexandra <alexandrafernandezquispe@gmail.com>
2016-08-25 15:28:29 -0500

    ejercicio 1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Ejercicio-1
+1. INICIO
+2. LEER DATOS   
+   - dato 1 
+   - dato 2
+3. SUMAR DATOS
+4. ENTREGAR RESULTADO
+5. FIN
+
+[![20160825_144047.jpg](https://s13.postimg.org/5ajm02tkn/20160825_144047.jpg)](https://postimg.org/image/lyb42koc3/)
---
+++ app.js
@@ -0,0 +1,5 @@
+var primerNumero = prompt("primer numero que se desea sumar");
+var segundoNumero = prompt("segundo numero que se desea sumar");
+var suma = parseInt(primerNumero)+parseInt(segundoNumero);
+window.alert("el resultado de la suma de ambos números es igual a");
+window.alert(suma);
